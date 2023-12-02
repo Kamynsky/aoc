73d7a106c4aea7cc28ac0cf276b35cebe0a43821
MK <mkamynsky@gmail.com>
2023-12-02 19:45:45 +0100

    Changed to file open from url file pool

--- day1puzzle1.py
+++ day1puzzle1.py
@@ -1,7 +1,4 @@
 #!~/usr/bin/python3
-import urllib.request
-
-url='https://adventofcode.com/2023/day/1/input'
-requestfile=urllib.request.urlopen(url)
-for line in requestfile:
-    print(line)
+with open('day1puzzle1.txt','r') as file:
+    for line in file: 
+        print(line)
