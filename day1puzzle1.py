ad572aad62bf1f6906cec64d8f8d65ad20a82ad4
Kamynsky <mkamynsky@gmail.com>
2023-12-02 23:06:15 +0100

    Day 1 puzzle 1 solved

--- day1puzzle1.py
+++ day1puzzle1.py
@@ -1,7 +1,18 @@
 #!~/usr/bin/python3
-import urllib.request
-
-url='https://adventofcode.com/2023/day/1/input'
-requestfile=urllib.request.urlopen(url)
-for line in requestfile:
-    print(line)
+import re
+sum=0
+pattern_two_digits=r'\D*(\d).*?(\d)\D*$'
+pattern_one_digit=r'\D*(\d).*'
+with open('input.txt','r') as file:
+    for line in file:
+        match = re.search(pattern_two_digits, line)
+        if match:
+            found_digits=match.group(1) + match.group(2)
+            print(found_digits)
+            sum=int(found_digits)+sum
+            print(sum)
+        else:
+            match = re.search(pattern_one_digit, line)
+            found_digits=match.group(1) + match.group(1)
+            sum=int(found_digits)+sum
+            print(sum)
