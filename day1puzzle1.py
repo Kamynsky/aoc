6340a60823f5a534b099bb7d3e38383891ed824e
Kamynsky <mkamynsky@gmail.com>
2023-12-03 11:57:26 +0100

    Removed input.txt , Added comments

--- day1puzzle1.py
+++ day1puzzle1.py
@@ -1,16 +1,17 @@
-#!~/usr/bin/python3
-import re
-sum=0
-pattern_two_digits=r'\D*(\d).*?(\d)\D*$'
-pattern_one_digit=r'\D*(\d).*'
-with open('input.txt','r') as file:
-    for line in file:
-        match = re.search(pattern_two_digits, line)
-        if match:
-            found_digits=match.group(1) + match.group(2)
-            sum=int(found_digits)+sum
-        else:
-            match = re.search(pattern_one_digit, line)
-            found_digits=match.group(1) + match.group(1)
-            sum=int(found_digits)+sum
-    print(sum)
+# Puzzle is to find first and last digit in the input file , if there is only one digit it is used twice as first and last digit
+import re #Import Regex Lib to search for Digits
+sum=0 # Initialize Sum which is needed for answer 
+pattern_two_digits=r'\D*(\d).*?(\d)\D*$' # Regex Pattern for Two digit search
+pattern_one_digit=r'\D*(\d).*' # Regex pattern for single digit search
+input_file='day1puzzle1.txt'
+with open(input_file,'r') as file: #open file containing strings 
+    for line in file: # go line by line 
+        match = re.search(pattern_two_digits, line) # search for first and last digit
+        if match: 
+            found_digits=match.group(1) + match.group(2) # If two digits are found combine them in to two digit number . 7 and 7 becomes 77
+            sum=int(found_digits)+sum # add to sum var for answer 
+        else: # if two digits are not found 
+            match = re.search(pattern_one_digit, line) # look for singe digit in the string
+            found_digits=match.group(1) + match.group(1) # combine one digit found in to 2 digit number . Meaning 5 becomes 55
+            sum=int(found_digits)+sum # add to the sum which is answer for to the puzzle
+    print(sum) # print answer
