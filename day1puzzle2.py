22d8e2d22ab52e1f2c545b807f0c2f9bf7613b2a
Kamynsky <mkamynsky@gmail.com>
2023-12-03 12:06:46 +0100

    Added word to digit dictionary

--- day1puzzle2.py
+++ day1puzzle2.py
@@ -5,6 +5,7 @@ sum=0 # Initialize Sum which is needed for answer
 pattern_two_digits=r'\D*(\d).*?(\d)\D*$' # Regex Pattern for Two digit search
 pattern_one_digit=r'\D*(\d).*' # Regex pattern for single digit search
 input_file='day1puzzle1.txt'
+word_to_digit={one:1,two:2,three:3,four:4,five:5,six:6,seven:7,eight:8,nine:9} # dictionary used for searching and replacing word to digit
 with open(input_file,'r') as file: #open file containing strings 
     for line in file: # go line by line 
         match = re.search(pattern_two_digits, line) # search for first and last digit
