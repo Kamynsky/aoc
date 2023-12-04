eba4f71dd85176ddd0db799d4514bf3c4eea5ed9
Kamynsky <mkamynsky@gmail.com>
2023-12-04 19:03:00 +0100

    Added input file, added chalange descritpion

---
+++ day2puzzle1.py
@@ -0,0 +1,4 @@
+# We are determinating which games are possible and then suming up Game ID 
+# 12 red cubes , 13 green cubes , 14 blue cubes are the max value
+# Colors are not sorted, number of cubes are always first , name of the color latter example 4 red, 10 blue , 12 green
+
