7ac2d08f29ca430fe21efc51fe6d89a6ee9d7c1c
Kamynsky <mkamynsky@gmail.com>
2023-12-06 20:17:37 +0100

    Added ? which fixed regex ;)

--- day2puzzle1.py
+++ day2puzzle1.py
@@ -8,9 +8,9 @@ red_max_value=12
 green_max_value=13
 blue_max_value=14
 pattern_game_id=r"(\d+):"
-pattern_cube_red_and_value=r".(\d+).red."
-pattern_cube_green_and_value=r'.(\d+).green.'
-pattern_cube_blue_and_value=r'.(\d+).blue.'
+pattern_cube_red_and_value=r".(\d+).red.?" #pattern for Value searching , that ?(0 or one char) was the issue ...
+pattern_cube_green_and_value=r'.(\d+).green.?'
+pattern_cube_blue_and_value=r'.(\d+).blue.?'
 with open('day2puzzle.txt','r') as input:
     for line in input:
         state=False
@@ -18,10 +18,10 @@ with open('day2puzzle.txt','r') as input:
         red=re.findall(pattern_cube_red_and_value,line)
         for value in red:
             int_value=int(value)
-            if int_value > red_max_value:
-                state=True  
+            if int_value > red_max_value: # edge condition
+                state=True  # state for breaking the loop if value is to high 
                 break
-        if state:
+        if state: # breaking higher loop so it doesn't check next color if one is already to high
             continue
             
         green=re.findall(pattern_cube_green_and_value,line)
@@ -43,8 +43,6 @@ with open('day2puzzle.txt','r') as input:
             continue
         
         if not state:
-            gameid=re.search(pattern_game_id,line)
-            print("Game ID to sum:",gameid.group(1))
-            sum=sum + int(gameid.group(1))
-            print("Current Sum :",sum)
-            print(line)
+            gameid=re.search(pattern_game_id,line) # get game id after checking for edge conditions 
+            sum=sum + int(gameid.group(1)) # sum game ID's 
+    print(sum) # print Game id 
