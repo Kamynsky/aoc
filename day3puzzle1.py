caa700d4da2a2ee9b21fc06bdce1db91898c317c
Kamynsky <mkamynsky@gmail.com>
2023-12-08 00:08:01 +0100

    some dumb ideas

--- day3puzzle1.py
+++ day3puzzle1.py
@@ -1,1 +1,14 @@
 # find numbers that are next to special char on the same line and +1 -1 index on the line over or below it
+import re
+input_file='day3puzzle.txt'
+speccharpattern=r'[^a-zA-Z0-9]'
+
+print(speccharpattern)
+with open(input_file,'r') as file: #open file containing strings 
+    for line in file: # go line by line
+        numberremover=str.maketrans('','','0123456789')
+        nodigi=line.translate(numberremover)
+        nodot=nodigi.replace('.','')
+        #print(nodot)
+        specchar=re.findall(speccharpattern,line)
+        print(specchar)
