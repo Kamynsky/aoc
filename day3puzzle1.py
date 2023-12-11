167ba0b5d0ba8086604972a45a4a26b6033e45ad
Kamynsky <mkamynsky@gmail.com>
2023-12-11 18:06:42 +0100

    .

--- day3puzzle1.py
+++ day3puzzle1.py
@@ -1,14 +1,18 @@
 # find numbers that are next to special char on the same line and +1 -1 index on the line over or below it
+# use index of numbers and special chars and comepere them to find adjecant ones , use try as a method to trying ?
+# we need to read 3 lines so we can compare middle to upper and below , this will ofc not work with first and last line 
+#or mayben we can go line by line and just look form indexes and compare them to next line?
 import re
 input_file='day3puzzle.txt'
 speccharpattern=r'[^a-zA-Z0-9]'
-
-print(speccharpattern)
-with open(input_file,'r') as file: #open file containing strings 
-    for line in file: # go line by line
-        numberremover=str.maketrans('','','0123456789')
-        nodigi=line.translate(numberremover)
-        nodot=nodigi.replace('.','')
-        #print(nodot)
-        specchar=re.findall(speccharpattern,line)
-        print(specchar)
+with open(input_file,'r') as file:
+    for line in file:
+        special_chars=line.replace('.','').rstrip()
+        special_chars=re.findall(speccharpattern,special_chars)
+        print(special_chars)
+#         for i, char in enumerate(line):
+#             if char.isdigit():
+#                 index_iterate = i
+#                 print(index_iterate)
+#             elif char==
+        
