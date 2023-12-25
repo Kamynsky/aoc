b3434a5e55c34f38232c6d0aa1e6ba140ca4a5f9
Kamynsky <mkamynsky@gmail.com>
2023-12-25 13:19:09 +0100

    Request is saved to file , name based on day var , check if file exist

--- day3puzzle1.py
+++ day3puzzle1.py
@@ -3,17 +3,22 @@
 # we need to read 3 lines so we can compare middle to upper and below , this will ofc not work with first and last line 
 #or mayben we can go line by line and just look form indexes and compare them to next line?
 import urllib.request
+import os
 day=str(3)
+input_file='inputday'+day+'.txt'
 def input_file_request():
-    coockie_file='c:/acoecoockie.txt'
-    with open(coockie_file,'r') as file:
-        coockie=file.read().strip()
-        url="http://adventofcode.com/2023/day/"+day+"/input"
-        request = urllib.request.Request(url)
-        request.add_header('Cookie', coockie)
-        response = urllib.request.urlopen(request)
-        return response
+    if os.path.exists(input_file):
+        print('File already exists')
+    else:
+        coockie_file='c:/acoecoockie.txt'
+        with open(coockie_file,'r') as file:
+            coockie=file.read().strip()
+            url="http://adventofcode.com/2023/day/"+day+"/input"
+            request = urllib.request.Request(url)
+            request.add_header('Cookie', coockie)
+            response = urllib.request.urlopen(request)
+            with open(input_file,'wb') as fileinput:
+                fileinput.write(response.read())
 response = input_file_request()
-input=response.read().decode('utf-8')
-print(input)
+
             
