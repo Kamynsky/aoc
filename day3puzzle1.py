d76dfcaffe20b10fd882012b87b29727057ca529
Kamynsky <mkamynsky@gmail.com>
2023-12-25 14:05:54 +0100

    Importing file as a grid

--- day3puzzle1.py
+++ day3puzzle1.py
@@ -2,12 +2,16 @@
 # use index of numbers and special chars and comepere them to find adjecant ones , use try as a method to trying ?
 # we need to read 3 lines so we can compare middle to upper and below , this will ofc not work with first and last line 
 #or mayben we can go line by line and just look form indexes and compare them to next line?
+
+#--------------------------------------------------------------------------------------------------------------------------
+#File download, day var set 
+#--------------------------------------------------------------------------------------------------------------------------
+#!/usr/bin/env python3
 import urllib.request
 import os
 file_with_path = os.path.abspath(__file__)
 file_name = os.path.basename(file_with_path)
 day=file_name[3]
-print(day)
 input_file='day'+day+'input.txt'
 def input_file_request():
     if os.path.exists(input_file):
@@ -24,4 +28,10 @@ def input_file_request():
                 fileinput.write(response.read())
 response = input_file_request()
 
-            
+#--------------------------------------------------------------------------------------------------------------------------
+#Code for puzzle
+#--------------------------------------------------------------------------------------------------------------------------
+with open(input_file,'r') as file:      # import file content as grid
+    for line in file.readlines():
+        grid.append(line.strip())
+
