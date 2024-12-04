64eae9950a828aaca9ab99c8353fc9e1bdbe4b81
Kamynsky <mkamynsky@gmail.com>
2024-12-04 08:44:03 +0100

    day2 attempt

--- day2solution.py
+++ day2solution.py
@@ -1,15 +1,28 @@
 # One line , one report = line.split()
 # Max diff between consequitve numbers <-/+3 , only increasing or decreasing vecotr
 # Solltuion is amount of safe reports
-i=0
+# numpy would be easier but lets do everything manually
+count=0
 with open("day2input.txt","r") as input:
     for line in input:
         line=line.split()
         lsize=(len(line))
-        for i in range(lsize):
-            i-=1
-            diff=int(line[i])-int(line[i+1])
-            i+=3
-        for i in range(len(line)):
-            diff=line[i]
+        print(line)
+        i=1
+        while i in range(lsize):
+            diff=int(line[i-1]) - int(line[i])
             print(diff)
+            i+=1
+            while diff in range(-3,3):
+                print("diff ok")
+                continue
+            else:
+                print("diff bad")
+            
+            
+
+
+
+
+
+            
